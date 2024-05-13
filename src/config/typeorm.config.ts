6fc6fd7d9662fc9613bebd9906091faac2ed7fbd
faizzulkarnaiin <faizmaqusa7@gmail.com>
2024-05-13 10:33:05 +0700

    ok

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,10 +2,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'localhost',
-  port: 3307, //port default 3306 lihat xampp
-  username: 'root', // username default xampp root
-  password: '', // password default xampp string kosong
-  database: 'project_nest_js',
+  port: 3307, 
+  username: 'root', 
+  password: '', 
+  database: 'nestjs_project_backup',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   logging: true,
