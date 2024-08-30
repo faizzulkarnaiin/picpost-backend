200366d0879b4b0bf01e2ec0db1d53f34ce55bea
faizzulkarnaiin <faizmaqusa7@gmail.com>
2024-08-30 09:27:36 +0700

    Remove ca.pem from repository

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,23 +1,23 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as fs from 'fs';
-import * as path from 'path';
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_USERNAME:', process.env.DB_USERNAME);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port:  parseInt(process.env.DB_PORT, 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
-  logging: true,
-  ssl: {
-    ca: fs.readFileSync(path.join(__dirname, './ca.pem')),
-    rejectUnauthorized: true,
-  },
-};
+  import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+  import * as fs from 'fs';
+  import * as path from 'path';
+  console.log('DB_HOST:', process.env.DB_HOST);
+  console.log('DB_PORT:', process.env.DB_PORT);
+  console.log('DB_USERNAME:', process.env.DB_USERNAME);
+  console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+  console.log('DB_DATABASE:', process.env.DB_DATABASE);
+  export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port:  parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: true,
+    logging: true,
+    ssl: {
+      ca: fs.readFileSync(path.join(__dirname, './ca.pem')),
+      rejectUnauthorized: true,
+    },
+  };
